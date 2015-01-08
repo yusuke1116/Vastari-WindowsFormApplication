f281456b5a04c40749c593ca6a1c80dfd3469ffc
yusuke1116 <yusuke.kawano@gmail.com>
2015-01-08 06:00:06 +0000

    8th January
    This is windows form application, not windows phone.
    I made this one in order to check how it work API.

---
+++ WindowsFormsApplication2/WindowsFormsApplication2/Form1.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace WindowsFormsApplication2
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            ServiceReference1.VastariAPIServiceClient client = new ServiceReference1.VastariAPIServiceClient();
+            var response = client.Login("yusuke", "shibainu48");
+            if (response != null && !string.IsNullOrEmpty(response.UserName))
+            {
+                MessageBox.Show("login suceess");
+            }
+            else
+            {
+                MessageBox.Show("fail");
+            }
+        }
+    }
+}
